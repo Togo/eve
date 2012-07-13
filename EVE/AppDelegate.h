c7537a3a2761e898fc137833eb8eeb4e46f10669
Tobias Sommer <togo210@googlemail.com>
2012-07-13 23:15:02 +0200

    license

--- EVE/AppDelegate.h
+++ EVE/AppDelegate.h
@@ -1,10 +1,24 @@
-//
-//  AppDelegate.h
-//  LearnShortcuts
-//
-//  Created by Tobias Sommer on 6/9/12.
-//  Copyright (c) 2012 Sommer. All rights reserved.
-//
+/*
+ AppDelegate.h
+ EVE
+ 
+ Created by Tobias Sommer on 6/13/12.
+ Copyright (c) 2012 Sommer. All rights reserved.
+ 
+ This file is part of EVE.
+ 
+ EVE is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ EVE is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with EVE.  If not, see <http://www.gnu.org/licenses/>. */
 
 #import <Cocoa/Cocoa.h>
 #import <HIServices/Accessibility.h>
--- EVE/AppDelegate.m
+++ EVE/AppDelegate.m
@@ -1,10 +1,25 @@
-//
-//  AppDelegate.h
-//  LearnShortcuts
-//
-//  Created by Tobias Sommer on 6/9/12.
-//  Copyright (c) 2012 Sommer. All rights reserved.
-//
+/*
+ AppDelegate.m
+ EVE
+ 
+ Created by Tobias Sommer on 6/13/12.
+ Copyright (c) 2012 Sommer. All rights reserved.
+ 
+ This file is part of EVE.
+ 
+ EVE is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ EVE is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with EVE.  If not, see <http://www.gnu.org/licenses/>. */
+
 
 #import <Cocoa/Cocoa.h>
 #import <AppKit/NSAccessibility.h>
@@ -24,13 +39,6 @@ NSString             *lastSendedShortcut;
 static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 
 @implementation AppDelegate
-
-//- (void)dealloc {
-//    if (_systemWideElement) CFRelease(_systemWideElement);
-//    if (_currentUIElement) CFRelease(_currentUIElement);
-//    if(shortcutDictionary) [shortcutDictionary dealloc];
-//    [super dealloc];
-//}
 
 
 - (void)applicationDidFinishLaunching:(NSNotification *)note {
