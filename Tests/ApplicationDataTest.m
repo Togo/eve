6c3f0135fc683b135aa4c77846393037fed405bf
Tobias Sommer <togo210@googlemail.com>
2012-07-28 12:40:38 +0200

    Add GHUnitTest Framework

---
+++ Tests/ApplicationDataTest.m
@@ -0,0 +1,54 @@
+//
+//  ApplicationDataTest.m
+//  EVE
+//
+//  Created by Tobias Sommer on 7/28/12.
+//  Copyright (c) 2012 Sommer. All rights reserved.
+//
+
+#import <GHUnit/GHUnit.h>
+
+@interface  ApplicationDataTest : GHTestCase { }
+@end
+
+@implementation  ApplicationDataTest
+
+- (BOOL)shouldRunOnMainThread {
+    // By default NO, but if you have a UI test or test dependent on running on the main thread return YES.
+    // Also an async test that calls back on the main thread, you'll probably want to return YES.
+    return NO;
+}
+
+- (void)setUpClass {
+    // Run at start of all tests in the class
+}
+
+- (void)tearDownClass {
+    // Run at end of all tests in the class
+}
+
+- (void)setUp {
+    // Run before each test method
+}
+
+- (void)tearDown {
+    // Run after each test method
+}   
+
+- (void)testFoo {       
+    NSString *a = @"foo";
+    GHTestLog(@"I can log to the GHUnit test console: %@", a);
+    
+    // Assert a is not NULL, with no custom error description
+   GHAssertNotNil(a, nil);
+    
+    // Assert equal objects, add custom error description
+    NSString *b = @"bar";
+    GHAssertEqualObjects(a, b, @"A custom error message. a should be equal to: %@.", b);
+}
+
+- (void)testBar {
+    // Another test
+}
+
+@end
